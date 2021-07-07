4fd451880e1b1b26fc39d7db45bb3c562d84cb2d
ZunayedDihan <zunayeddihan@gmail.com>
2021-07-07 06:26:54 +0000

    daisy: aicp !

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_daisy.mk
+    $(LOCAL_DIR)/aicp_daisy.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_daisy-user \
-    derp_daisy-userdebug \
-    derp_daisy-eng
+    aicp_daisy-user \
+    aicp_daisy-userdebug \
+    aicp_daisy-eng
