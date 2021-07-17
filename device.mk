911b5e8f778251eea2c4e15e0caf52667e5a1d4a
Chenyang Zhong <zhongcy95@gmail.com>
2021-07-17 22:09:55 +0000

    daisy: enable APEX Updates and create and symlink a dummy /metadata/apex
     * Google moved apex sessions directory from /data/apex/sessions to
    /metadata/apex/sessions after commit:
    
    "Move apex sessions directory to /metadata"
    https://android.googlesource.com/platform/system/apex/+/36cf4bbac6108898a531aacc102dcc489d791b37
    
    Devices with a mounted metadata partition will have the needed
    directories set up by system/core/rootdir/init.rc. Xiaomi devices
    on sm6125 do not have a metadata partition out of the box, so things
    like "Google Play system update" will fail to install the update.
    
    Therefore, create a dummy directory under /data/vendor/metadata_apex
    and symlink it to /metadata/apex.
    
    The reason why the old /data/apex/sessions directory is not used
    for the symlink is that apexd will call migrateSessionsDirIfNeeded()
    to recursively copy things from the old directory to the new one.
    Creating the symlink from /data/apex/sessions may result in
    unintended behaviors.
    
    Signed-off-by: ZunayedDihan <zunayeddihan@gmail.com>

--- device.mk
+++ device.mk
@@ -17,7 +17,7 @@
 $(call inherit-product, vendor/xiaomi/daisy/daisy-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Enable updating of APEXes
+# APEX
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Setup dalvik vm configs
