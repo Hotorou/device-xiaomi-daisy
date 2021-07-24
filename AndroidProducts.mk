af78ddb82b43755fd4c233f406804b7fe7c10387
vitor00almei <pondebatata@outlook.com>
2021-07-24 04:38:20 +0000

    sakura: rebrand tree for daisy

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,4 +14,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/octavi_sakura.mk
+    $(LOCAL_DIR)/octavi_daisy.mk
--- octavi_daisy.mk
+++ octavi_daisy.mk
@@ -24,21 +24,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 
-# Inherit some prebuilts
-$(call inherit-product, vendor/custom/prebuilts/config.mk)
-
-# Inherit from mido device
-$(call inherit-product, device/xiaomi/sakura/device.mk)
+# Inherit from daisy device
+$(call inherit-product, device/xiaomi/daisy/device.mk)
 
 # Inherit some common OctaviOS stuff.
 $(call inherit-product, vendor/octavi/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := sakura
-PRODUCT_NAME := octavi_sakura
+PRODUCT_DEVICE := daisy
+PRODUCT_NAME := octavi_daisy
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Redmi 6 Pro
+PRODUCT_MODEL := Mi A2 Lite
 PRODUCT_MANUFACTURER := Xiaomi
 TARGET_VENDOR := Xiaomi
 
