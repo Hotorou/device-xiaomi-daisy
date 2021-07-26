bc3a47c21e273d7648f53b5a911b41af2102ceba
Hotorou <84888277+Hotorou@users.noreply.github.com>
2021-07-26 13:50:47 +0600

    not needed as of now

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,8 +4,6 @@ git clone --depth=1 https://github.com/LineageOS/android_hardware_qcom_media.git
 
 git clone --depth=1 https://github.com/LineageOS/android_hardware_qcom_audio.git -b lineage-18.1-caf-msm8996 vendor/qcom/opensource/audio-hal/primary-hal
 
-git clone --depth=1 https://github.com/lupesoltec/vendor_MiuiCamera vendor/MiuiCamera
-
 rm-rf hardware/qcom-caf/bootctrl && git clone --depth=1 https://github.com/LineageOS/android_hardware_qcom_bootctrl.git hardware/qcom-caf/bootctrl
 
 rm -rf vendor/qcom/opensource/power && git clone --depth=1 https://github.com/AOSPA/android_vendor_qcom-opensource_power.git vendor/qcom/opensource/power
