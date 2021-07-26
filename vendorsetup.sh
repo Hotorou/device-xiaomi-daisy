10ae54387fc62b529f5a324829b8c7135e88e726
Hotorou <84888277+Hotorou@users.noreply.github.com>
2021-07-26 20:21:54 +0600

    vendorsetup: set correct path destination of hals
    Co-authored-by: V1t0rrr <V1t0rrr@users.noreply.github.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,9 @@
-git clone --depth=1 https://github.com/vitor00almei/android_hardware_qcom_display hardware/qcom/display
+git clone https://github.com/vitor00almei/android_hardware_qcom_display hardware/qcom-caf/msm8996/display
 
-git clone --depth=1 https://github.com/LineageOS/android_hardware_qcom_media.git -b lineage-18.1-caf-msm8996 hardware/qcom/media
+git clone https://github.com/LineageOS/android_hardware_qcom_media.git -b lineage-18.1-caf-msm8996 hardware/qcom-caf/msm8996/media
 
-git clone --depth=1 https://github.com/LineageOS/android_hardware_qcom_audio.git -b lineage-18.1-caf-msm8996 vendor/qcom/opensource/audio-hal/primary-hal
+git clone https://github.com/LineageOS/android_hardware_qcom_audio.git -b lineage-18.1-caf-msm8996 vendor/qcom/opensource/audio-hal/primary-hal
 
-rm-rf hardware/qcom-caf/bootctrl && git clone --depth=1 https://github.com/LineageOS/android_hardware_qcom_bootctrl.git hardware/qcom-caf/bootctrl
+rm-rf hardware/qcom-caf/bootctrl && git clone https://github.com/LineageOS/android_hardware_qcom_bootctrl.git hardware/qcom-caf/bootctrl
 
-rm -rf vendor/qcom/opensource/power && git clone --depth=1 https://github.com/AOSPA/android_vendor_qcom-opensource_power.git vendor/qcom/opensource/power
+rm -rf vendor/qcom/opensource/power && git clone https://github.com/AOSPA/android_vendor_qcom-opensource_power.git vendor/qcom/opensource/power
